da83781956df8206c28c35cfb547fd9260c30fb6
AbhijitSarker <abhijitsarker03@gmail.com>
2023-11-28 00:16:11 +0600

    db connection setup

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect');
+require('dotenv').config()
 
 //middlewares
 
@@ -21,5 +22,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGOOSE_URI)
+        app.listen(port, () => console.log(`Server is running at ${port}...`));
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-app.listen(port, console.log(`Server is running at ${port}...`));
+start();
---
+++ db/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+
+
+
+const connectDB = (url) => {
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        // useFindAndModifyIndex: false,
+        useUnifiedTopology: true,
+    })
+}
+
+
+module.exports = connectDB
