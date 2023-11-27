6978c5bbc78f6e73274ab608377562220328b835
AbhijitSarker <abhijitsarker03@gmail.com>
2023-11-28 02:09:31 +0600

    update a task

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -34,10 +34,6 @@ const getTask = async (req, res) => {
     }
 };
 
-const updateTask = (req, res) => {
-    res.send('Update a Task');
-};
-
 const deleteTask = async (req, res) => {
     try {
         const { id: taskID } = req.params;
@@ -53,7 +49,25 @@ const deleteTask = async (req, res) => {
     }
 };
 
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
 
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+            new: true,
+            runValidators: true,
+        })
+
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id : ${taskID}` });
+        }
+        res.status(200).json({ task });
+    } catch (err) {
+        res.status(500).json({ msg: err })
+
+    }
+
+}
 
 module.exports = {
     getAllTasks,
